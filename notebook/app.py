2ebdb0d973f680c6132214d9d7db4596be37ee8d
BrunoGeraldine <databrunog@gmail.com>
2023-06-07 14:56:52 -0300

    new path

--- notebook/app.py
+++ notebook/app.py
@@ -18,7 +18,7 @@ n_dias = st.slider('Quantidade de dias a ser prevista', 30, 365)
 
 #Coletando o nome e sigla da ação escolhida
 def pegar_dados_acao():
-    path = '/home/bruno/repos/B3_Prediction_share_bmg_2023/dataset/acao_nome.csv'
+    path = '../dataset/acao_nome.csv'
     return pd.read_csv(path)
 
 df = pegar_dados_acao()
